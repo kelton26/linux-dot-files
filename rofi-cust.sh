9139af58c6c69f9954d00a4f6a256a8f4ce85aed
Kelton Antao <keltonant@gmail.com>
2026-04-30 01:32:17 +0530

    Add files via upload

---
+++ rofi-cust.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WALL1="/home/kelton/wallpapers/wall.jpg"
+WALL2="/home/kelton/wallpapers/wall1.jpg"
+
+chosen=$(printf \
+  "Wallpaper 1\0icon\x1f%s\nWallpaper 2\0icon\x1f%s\nbtop\nKill all\nFans\nKeyboard\n" \
+  "$WALL1" "$WALL2" |
+  rofi -dmenu -p "Switch Wallpaper" -show-icons)
+
+case "$chosen" in
+"Wallpaper 1")
+  pkill -f 'kitten panel'
+  awww-daemon
+  awww img "$WALL1" --transition-type random --transition-fps=60
+  ;;
+"Wallpaper 2")
+  pkill -f 'kitten panel'
+  awww-daemon
+  awww img "$WALL2" --transition-type random --transition-fps=60
+  ;;
+"btop")
+  kitten panel --edge=background btop
+  ;;
+"Kill all")
+  pkill awww-daemon
+  pkill -f 'kitten panel'
+  ;;
+"Fans")
+  hyprctl 'dispatch exec [float] nbfc-gtk'
+  ;;
+"Keyboard")
+  kitty zsh -c "python ~/acer-predator-turbo-and-rgb-keyboard-linux-module/keyboard.py; exec zsh"
+  ;;
+esac
